968fa95b9127555767d168c8e3f8071c487217e0
mib1092 <iigore4ekk@gmail.com>
2016-12-21 01:43:38 +0200

    html1

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,3 +1,10 @@
 jQuery(document).ready(function($) {
 
+    // for header-fixed padding
+    $('.wrapper').css('padding-top', $('#header').outerHeight());
+
+    $(window).scroll(function(){
+        $('#header').toggleClass('scroll-js', $(this).scrollTop() > 0);
+    });
+
 });
