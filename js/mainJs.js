491ecc5afb974a6c38ea013baac29eecff91d7e2
mib1092 <iigore4ekk@gmail.com>
2016-12-21 03:23:18 +0200

    html3

--- js/mainJs.js
+++ js/mainJs.js
@@ -7,4 +7,11 @@ jQuery(document).ready(function($) {
         $('#header').toggleClass('scroll-js', $(this).scrollTop() > 0);
     });
 
+    // for smooth scroll
+    smoothScroll.init({
+        selector: '[data-scroll], .navigation a', // Selector for links (must be a class, ID, data attribute, or element tag)
+        speed: 500, // Integer. How fast to complete the scroll in milliseconds
+        easing: 'easeInQuad', // Easing pattern to use
+        offset: 50 // Integer. How far to offset the scrolling anchor location in pixels
+    });
 });
